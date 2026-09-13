3df4d312fa4e8732ec279f188b392f1f6c81823e
Rahul Krishna <your-github-rahulkrahul5555@gmail.com>
2026-09-13 20:57:21 +0530

    Align staging models with python mock data columns

--- marketing_dbt/models/marketing/staging/stg_transactions.sql
+++ marketing_dbt/models/marketing/staging/stg_transactions.sql
@@ -1,6 +1,6 @@
 SELECT
-    order_id,
-    user_id,                  -- <-- Make sure user_id is right here
+    transaction_id AS order_id,  -- Maps the python script's column
+    user_id,
     purchase_timestamp AS purchased_at,
     order_amount AS amount_usd
 FROM {{ source('marketing', 'raw_transactions') }}
--- marketing_dbt/models/marketing/staging/stg_web_clicks.sql
+++ marketing_dbt/models/marketing/staging/stg_web_clicks.sql
@@ -1,5 +1,5 @@
 SELECT
-    session_id,
+    click_id AS session_id,      -- Maps the python script's column
     click_timestamp AS clicked_at,
     {{ dbt_utils.generate_surrogate_key(['utm_source']) }} AS utm_source_key,
     utm_source,
