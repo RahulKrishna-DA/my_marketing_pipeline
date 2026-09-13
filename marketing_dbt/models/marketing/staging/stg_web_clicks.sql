74b380070b8a6e186a4cbcd93b38f2335427a25b
Rahul Krishna <your-github-rahulkrahul5555@gmail.com>
2026-09-13 22:09:24 +0530

    Lock in exact matching python script and staging columns

--- marketing_dbt/models/marketing/staging/stg_web_clicks.sql
+++ marketing_dbt/models/marketing/staging/stg_web_clicks.sql
@@ -1,6 +1,6 @@
 SELECT
     click_id AS session_id,
-    click_timestamp AS clicked_at,
+    timestamp AS clicked_at,
     campaign_id,
     {{ dbt_utils.generate_surrogate_key(['campaign_id']) }} AS utm_source_key,
     campaign_id AS utm_source,
