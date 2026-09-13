d3a1367887c6020a81ec27e873a7acd58c460a72
Rahul Krishna <your-github-rahulkrahul5555@gmail.com>
2026-09-13 21:19:44 +0530

    Fix staging column mappings for transactions and web clicks

--- marketing_dbt/models/marketing/staging/stg_web_clicks.sql
+++ marketing_dbt/models/marketing/staging/stg_web_clicks.sql
@@ -1,9 +1,7 @@
 SELECT
-    click_id AS session_id,
-    timestamp AS clicked_at,
-    campaign_id,
-    {{ dbt_utils.generate_surrogate_key(['campaign_id']) }} AS utm_source_key,
-    campaign_id AS utm_source,
-    visitor_id AS user_id,
-    converted
+    session_id,
+    click_timestamp AS clicked_at,
+    {{ dbt_utils.generate_surrogate_key(['utm_source']) }} AS utm_source_key,
+    utm_source,
+    user_id
 FROM {{ source('marketing', 'raw_web_clicks') }}
